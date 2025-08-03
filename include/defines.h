fcdae8d713185535e6b7a758ff11ee11783bbfe9
bt-soft <bt-soft@freemail.hu>
2025-08-03 17:04:57 +0200

    Refactor code structure for improved readability and maintainability

--- include/defines.h
+++ include/defines.h
@@ -36,7 +36,7 @@
 #define MEMORY_INFO_INTERVAL 20 * 1000 // 20mp
 
 // Soros portra várakozás a debug üzenetek előtt
-#define DEBUG_WAIT_FOR_SERIAL
+//#define DEBUG_WAIT_FOR_SERIAL
 
 // Debug keretek rajzolása a UI komponensek köré
 #define DRAW_DEBUG_FRAMES
