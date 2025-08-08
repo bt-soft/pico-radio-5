c741b9d7ab5330bdf51a87f0ec3119ed2b284bda
bt-soft <bt-soft@freemail.hu>
2025-08-08 22:36:33 +0200

    Add lock files for PCB and schematic of pico-radio-5 project

--- include/pins.h
+++ include/pins.h
@@ -15,13 +15,13 @@
 #define PIN_VBUS_EXTERNAL_MEASURE_INPUT A1 // A1/GPIO27 a VBUS mérésének feszültségosztós bemenetéhez
 
 // Rotary Encoder
-#define PIN_ENCODER_CLK 17
 #define PIN_ENCODER_DT 16
+#define PIN_ENCODER_CLK 17
 #define PIN_ENCODER_SW 18
 
 // Others
-#define PIN_TFT_BACKGROUND_LED 21
 #define PIN_AUDIO_MUTE 20
+#define PIN_TFT_BACKGROUND_LED 21
 #define PIN_BEEPER 22
 
 #endif // __PINS_H
